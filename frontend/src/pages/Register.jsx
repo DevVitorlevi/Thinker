4a3cf0863e1ad46161155269fdac8d8e58af9c69
Vitor Levi <160429477+DevVitorlevi@users.noreply.github.com>
2025-02-09 09:58:54 -0300

    att

---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+
+const Register = () => {
+    return (
+        <div className="container">
+            <div className="image">
+
+            </div>
+            <div className='form'>
+
+            </div>
+        </div>
+    )
+}
+
+export default Register
