d71f550ab631bfe8c2c22a3773224a07c39c31f6
Vitor Levi <160429477+DevVitorlevi@users.noreply.github.com>
2025-02-21 22:24:11 -0300

    Feito a Estrutura do Form e a alguns estilos
    Todos, Restam Somente as Media Queries

--- frontend/src/pages/auth/Login.tsx
+++ frontend/src/pages/auth/Login.tsx
@@ -1,9 +1,66 @@
-
+import { Eye, EyeClosed } from "lucide-react"
+import { useState, useRef } from "react"
+import { Container, Imagediv, Formdiv, Divider, Text, Form, Auth } from '../../styles/Form'
+import image1 from '../../assets/image1.png'
+import image2 from '../../assets/image2.png'
+import { Link } from "react-router-dom";
 
 const Login = () => {
     return (
-        <div>Login</div>
-    )
-}
+        <>
+            <Container>
+                <Imagediv>
+                    <h1 className='font'>THINKER</h1>
+                    <img src={image1} alt="Thinker logo" className='mobile' />
+                    <img src={image2} className='desktop' />
+                </Imagediv>
+                <Formdiv>
+                    <h1 className='titulo'>Conecte-se ao THINKER</h1>
+                    <Auth>
+                        <div className="google">
+                            <p>Entre com o Google</p>
+                        </div>
+
+                    </Auth>
+                    <Divider>
+                        <Text>ou com e-mail</Text>
+                    </Divider>
+                    <Form>
+                        <form>
+                            <div className="inputs">
+                                <p>Email</p>
+                                <input
+                                    type="text"
+                                    name='email'
+                                    className='input '
+                                    placeholder='Insira seu endereço de e-mail'
+                                    required
+                                />
+                            </div>
+                            <div className="inputs">
+                                <p>Senha</p>
+                                <input
+                                    type="password"
+                                    name="password"
+                                    className='input'
+                                    required
+                                    placeholder='Sua Senha'
+                                />
+
+                                <Link to='/forgot' id='forgot'>Esqueci a Senha</Link>
+                            </div>
+
+                            <div className="buttons">
+                                <button type="submit">Entre</button>
+
+                                <Link to='/register' id='link'>Novo no THINKER? Criar uma conta</Link>
+                            </div>
+                        </form>
+                    </Form>
+                </Formdiv>
+            </Container>
+        </>
+    );
+};
 
 export default Login
--- frontend/src/pages/auth/Register.tsx
+++ frontend/src/pages/auth/Register.tsx
@@ -1,10 +1,65 @@
 
-
+import { use, useRef, useState } from 'react'
+import image1 from '../../assets/image2.png'
+import image2 from '../../assets/image1.png'
+import { Container, Imagediv, Formdiv, Divider, Text, Form, Auth } from '../../styles/Form'
+import { Eye, EyeClosed } from 'lucide-react'
+import { Link } from 'react-router-dom'
 
 
 const Register = () => {
+    const [open, setopen] = useState(false)
     return (
-        <div>Register</div>
+        <>
+            <Container>
+                <Imagediv>
+                    <h1 className='font'>THINKER</h1>
+                    <img src={image2} className='mobile' />
+                    <img src={image1} alt="Thinker logo" className='desktop' />
+                </Imagediv>
+                <Formdiv>
+                    <div className="entre">
+                        <h1 className='titulo'>Junte-se a THINKER</h1>
+                    </div>
+                    <Auth>
+                        <div className="google">
+                            <p>Entre com o Google</p>
+                        </div>
+                    </Auth>
+                    <Divider>
+                        <Text>ou com e-mail</Text>
+                    </Divider>
+                    <Form>
+                        <form>
+                            <div className="inputs">
+                                <p> Nome do Usuário </p>
+                                <input
+                                    type="text"
+
+                                    name='name'
+                                    className='input'
+                                    placeholder='Seu Nome'
+                                    required
+                                />
+
+                            </div>
+                            <div className="inputs">
+                                <p>Email</p>
+                                <input type="text" className='input'
+                                    placeholder='Insire seu enderenço de e-mail' />
+                            </div>
+                            <div className="inputs">
+                                <p>Senha</p>
+                                <input type="password"
+                                    placeholder='Digite sua Senha' className='input' />
+                            </div>
+                            <button type="submit">Cadastre-se</button>
+                            <Link to='/login' target='_blank' id='link'>Já Possui Conta? Entre</Link>
+                        </form>
+                    </Form>
+                </Formdiv>
+            </Container>
+        </>
     )
 }
 
