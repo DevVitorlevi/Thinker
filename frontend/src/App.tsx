c55a0343723b4469607ec8740ab1b46e9674d659
Vitor Levi <160429477+DevVitorlevi@users.noreply.github.com>
2025-02-21 21:10:16 -0300

    Estrururade paginas e pastas feitas

---
+++ frontend/src/App.tsx
@@ -0,0 +1,25 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+//Pages
+//auth
+import Login from './pages/auth/Login'
+import Register from './pages/auth/Register'
+import Home from "./pages/Home"
+import NotFound from "./pages/NotFound"
+
+function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
