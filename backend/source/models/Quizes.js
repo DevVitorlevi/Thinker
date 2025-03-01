8fa7c514a2495263fc63ed93bc6a1d8ca3b500b9
Vitor Levi <160429477+DevVitorlevi@users.noreply.github.com>
2025-03-01 20:01:21 -0300

    Update Quizes.js
    MODEL

--- backend/source/models/Quizes.js
+++ backend/source/models/Quizes.js
@@ -1,11 +1,9 @@
 const mongoose = require('mongoose');
-const { Schema } = mongoose;
 
-const Quiz = new Schema({
+const Quiz = new mongoose.Schema({
     titulo: { type: String, required: true },
-    descricao: { type: String },
-    materia: { type: mongoose.Schema.Types.ObjectId, ref: 'Materia', required: true }, // Referência para a matéria
-    questoes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Questao' }] // Lista de questões associadas ao quiz
+    materia: { type: mongoose.Schema.Types.ObjectId, ref: 'Materia' },
+    questoes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Questao' }]
 }, { timestamps: true });
 
 module.exports = mongoose.model('Quiz', Quiz);
