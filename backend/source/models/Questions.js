ca6a1f129bdac258cb50c6ab8bb748bdae32edaa
Vitor Levi <160429477+DevVitorlevi@users.noreply.github.com>
2025-03-01 20:01:12 -0300

    Update Questions.js
    model

--- backend/source/models/Questions.js
+++ backend/source/models/Questions.js
@@ -1,13 +1,11 @@
 const mongoose = require('mongoose');
-const { Schema } = mongoose;
 
-const Questao = new Schema({
-    enunciado: { type: String, required: true },
-    alternativa_a: { type: String, required: true },
-    alternativa_b: { type: String, required: true },
-    alternativa_c: { type: String, required: true },
-    alternativa_d: { type: String, required: true },
-    alternativa_correta: { type: String, required: true } // 'A', 'B', 'C' ou 'D'
+
+const Questao = new mongoose.Schema({
+    pergunta: { type: String, required: true },
+    alternativas: [{ type: String, required: true }],
+    respostaCorreta: { type: String, required: true },
+    quiz: { type: mongoose.Schema.Types.ObjectId, ref: 'Quiz' }
 }, { timestamps: true });
 
 module.exports = mongoose.model('Questao', Questao);
