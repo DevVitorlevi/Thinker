821b3829c13be398b92d950ecaffce6400871878
Vitor Levi <160429477+DevVitorlevi@users.noreply.github.com>
2025-03-05 13:28:48 -0300

    Update User.js

--- backend/source/models/User.js
+++ backend/source/models/User.js
@@ -1,10 +1,7 @@
 const mongoose = require('mongoose');
 
 const EstatsticasSchema = new mongoose.Schema({
-    questoes_feitas: [{
-        dificuldade: { type: String, enum: ['facil', 'medio', 'dificil'], required: true },
-        quantidade: { type: Number, default: 0 }
-    }],
+    questoes_feitas: { type: Number, default: 0 },
     acertos: { type: Number, default: 0 },
     quizzes_completos: { type: Number, default: 0 },
 });
@@ -14,14 +11,16 @@ const UserSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
     senha: { type: String, required: true },
     image: { type: String },
-    role: { type: String, enum: ['admin', 'user'], default: 'user' },
+    role: { type: String, enum: ['admin', 'user'], default: 'user' }, // Define se é admin ou usuário normal
     estatisticas: { type: EstatsticasSchema, default: () => ({}) },
     quizzes_respondidos: [{
         quiz: { type: mongoose.Schema.Types.ObjectId, ref: 'Quiz' },
         data: { type: Date, default: Date.now },
         acertos: { type: Number, default: 0 },
         total_questoes: { type: Number, default: 0 }
-    }]
+    }],
+    pontos: { type: Number, default: 0 }, // Pontos do usuário
+    ranking: { type: String, default: 'Aprendiz Curioso' } // Ranking atual do usuário
 }, { timestamps: true });
 
 module.exports = mongoose.model('User', UserSchema);
