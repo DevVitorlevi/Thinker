a33187321e453583b542baa2885d8cbe1d119074
Vitor Levi <160429477+DevVitorlevi@users.noreply.github.com>
2025-03-05 17:30:31 -0300

    Create App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,40 @@
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+// Pages
+import Login from './pages/auth/Login';
+import Register from './pages/auth/Register';
+import Home from './pages/home/Home';
+// Style
+import { GlobalStyle } from "./styles/Global";
+// Components
+import Header from "./components/Header";
+
+// App component
+function App() {
+  return (
+    <BrowserRouter>
+      <GlobalStyle />
+      <Routes>
+        <Route path="/*" element={<Layout />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+// Layout component (responsável pela renderização do Header de acordo com a rota)
+function Layout() {
+  const location = useLocation();
+  const showHeader = location.pathname !== '/register' && location.pathname !== '/login';
+
+  return (
+    <>
+      {showHeader && <Header />}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
