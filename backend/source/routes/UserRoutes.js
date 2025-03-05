872f260ffe73fa099d113c75212e3e62654dcf12
Vitor Levi <160429477+DevVitorlevi@users.noreply.github.com>
2025-03-05 13:28:57 -0300

    Update UserRoutes.js

--- backend/source/routes/UserRoutes.js
+++ backend/source/routes/UserRoutes.js
@@ -3,8 +3,9 @@ const router = express.Router();
 const UserController = require('../controllers/UserController');
 const VerifyToken = require('../helpers/verify-token'); // Middleware para verificar token
 const { ImageUpload } = require('../helpers/image-up'); // Middleware para upload de imagem
-const QuestaoController = require('../controllers/QuestionsController')
-const QuizController = require('../controllers/QuizController')
+const QuestaoController = require('../controllers/QuestionsController');
+const QuizController = require('../controllers/QuizController');
+const RankingController = require('../controllers/RankingController');
 
 // Registrar um novo usuário
 router.post('/register', UserController.register);
@@ -21,6 +22,16 @@ router.get('/:id', VerifyToken, UserController.getUser);
 // Editar informações do usuário (foto de perfil)
 router.patch('/edit/:id', VerifyToken, ImageUpload.single('image'), UserController.editUser);
 
+// Responder questão
 router.post('/responder', VerifyToken, QuestaoController.responderQuestao);
+
+// Completar quiz
 router.post('/completar-quiz', VerifyToken, QuizController.completarQuiz);
+
+// Obter rankings
+router.get('/ranking/list', VerifyToken, RankingController.getRankings);
+
+// Obter ranking do usuário
+router.get('/ranking/user', VerifyToken, RankingController.getUserRanking);
+
 module.exports = router;
