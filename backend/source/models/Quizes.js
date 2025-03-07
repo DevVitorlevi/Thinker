e3347beea8251fe168e46d4619e0152d97e73a96
Vitor Levi <160429477+DevVitorlevi@users.noreply.github.com>
2025-03-06 21:59:22 -0300

    Update Quizes.js

--- backend/source/models/Quizes.js
+++ backend/source/models/Quizes.js
@@ -4,7 +4,15 @@ const Quiz = new mongoose.Schema({
     titulo: { type: String, required: true },
     materia: { type: mongoose.Schema.Types.ObjectId, ref: 'Materia' },
     questoes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Questao' }],
-    tempo_estimado: { type: Number, default: 0 } // Tempo estimado em minutos para conclusão
+    completado_por: [{
+        user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+        data: { type: Date, default: Date.now },
+        acertos: { type: Number, default: 0 },
+        total_questoes: { type: Number, default: 0 },
+        pontos_obtidos: { type: Number, default: 0 } // Pontos obtidos no quiz
+    }],
+    tempo_estimado: { type: Number, default: 0 }, // Tempo estimado em minutos para conclusão
+    pontuacao_maxima: { type: Number, default: 250 } // Pontuação máxima do quiz
 }, { timestamps: true });
 
 module.exports = mongoose.model('Quiz', Quiz);
