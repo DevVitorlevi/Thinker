3fc46fe753001734c7bc51f7f9eaa9b7296e4890
Vitor Levi <160429477+DevVitorlevi@users.noreply.github.com>
2025-03-06 21:59:38 -0300

    Update Ranking.js

--- backend/source/models/Ranking.js
+++ backend/source/models/Ranking.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 const RankingSchema = new mongoose.Schema({
     nome: { type: String, required: true, unique: true }, // Nome do ranking (ex: "Aprendiz Curioso")
-    pontosNecessarios: { type: Number, required: true } // Pontos necessários para alcançar este ranking
+    pontosNecessarios: { type: Number, required: true }, // Pontos necessários para alcançar este ranking
+    insignia: { type: String, required: true } 
 }, { timestamps: true });
 
 module.exports = mongoose.model('Ranking', RankingSchema);
