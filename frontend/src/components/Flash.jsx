81b35702885524a94f51437dff13c18bd631af97
Vitor Levi <160429477+DevVitorlevi@users.noreply.github.com>
2025-03-08 14:59:09 -0300

    Update Flash.jsx

--- frontend/src/components/Flash.jsx
+++ frontend/src/components/Flash.jsx
@@ -1,13 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 import { Message } from '../styles/Flash';
+import bus from '../utils/bus';
 
 const Flash = () => {
-    const [type, setType] = React.useState('');
-    return (
-        <Message>
-            Mensage
-        </Message>
-    )
-}
+    const [type, setType] = useState('');
+    const [visible, setVisible] = useState(false);
+    const [message, setMessage] = useState('');
 
-export default Flash
+    useEffect(() => {
+
+        const handleFlash = ({ message, type }) => {
+            console.log("Evento recebido no Flash.js:", message, type);
+            setMessage(message);
+            setType(type);
+            setVisible(true);
+
+            setTimeout(() => {
+                setVisible(false);
+            }, 3000);
+        };
+
+
+        bus.addListener('flash', handleFlash);
+
+        return () => {
+            bus.removeListener('flash', handleFlash);
+        };
+    }, []);
+
+    return (
+        <div>
+            {visible && (
+                <Message className={`${type} ${visible ? 'visible' : ''}`}>
+                    <p>{message}</p>
+                </Message>
+
+            )}
+        </div>
+    );
+};
+
+export default Flash;
