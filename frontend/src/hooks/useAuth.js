af09335099c6dd29c3133ed1f97ebe855e73f260
Vitor Levi <160429477+DevVitorlevi@users.noreply.github.com>
2025-03-08 17:01:27 -0300

    Update useAuth.js

--- frontend/src/hooks/useAuth.js
+++ frontend/src/hooks/useAuth.js
@@ -1,23 +1,40 @@
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';  // Adicione esta linha
 import api from '../utils/api';
 import useFlash from './useFlash';
 
 export default function useAuth() {
     const { setFlash } = useFlash();
+    const [authed, setAuthed] = useState(false);  // Corrigido o uso do useState
+    const navigate = useNavigate();
+
+    useEffect(()=>{
+        const token = localStorage.getItem('token');
+        if (token) {
+            api.defaults.headers.common['Authorization'] = `Bearer ${JSON.parse(token)}`;
+            setAuthed(true);
+        }
+    },[])
 
     async function register(user) {
-        console.log("Enviando para o backend:", user); // Verificar os dados
-    
         try {
             const response = await api.post('/user/register', user);
             setFlash('Cadastro realizado com sucesso!', 'success');
+            await authUser(response.data);  // Chame authUser com os dados recebidos
             return response.data;
+
         } catch (error) {
-            console.log("Erro no cadastro:", error.response?.data);
             const errorMessage = error.response?.data?.message || 'Erro ao cadastrar';
             setFlash(errorMessage, 'error');
             return null;
         }
     }
 
-    return { register };
+    async function authUser(data) {
+        setAuthed(true);
+        localStorage.setItem('token', JSON.stringify(data.token));
+        navigate('/');  // Navegação após autenticação
+    }
+
+    return {authed, register };
 }
