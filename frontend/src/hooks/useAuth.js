4cfdb15c7a815af63d5afbb2dda815e49c09e29a
Vitor Levi <160429477+DevVitorlevi@users.noreply.github.com>
2025-03-08 14:59:13 -0300

    Update useAuth.js

--- frontend/src/hooks/useAuth.js
+++ frontend/src/hooks/useAuth.js
@@ -1,16 +1,23 @@
-// API
 import api from '../utils/api';
+import useFlash from './useFlash';
 
 export default function useAuth() {
+    const { setFlash } = useFlash();
+
     async function register(user) {
+        console.log("Enviando para o backend:", user); // Verificar os dados
+    
         try {
             const response = await api.post('/user/register', user);
-            return response.data; // Retorna os dados da resposta
+            setFlash('Cadastro realizado com sucesso!', 'success');
+            return response.data;
         } catch (error) {
-            console.error("Erro ao registrar:", error.response?.data || error.message);
-            return null; // Retorna null ou pode lançar um erro personalizado
+            console.log("Erro no cadastro:", error.response?.data);
+            const errorMessage = error.response?.data?.message || 'Erro ao cadastrar';
+            setFlash(errorMessage, 'error');
+            return null;
         }
     }
-    
+
     return { register };
 }
