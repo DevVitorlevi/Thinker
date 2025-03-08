079e73523144a56a672955dcb2595ae1687c2e6a
Vitor Levi <160429477+DevVitorlevi@users.noreply.github.com>
2025-03-08 14:59:16 -0300

    Update useFlash.js

--- frontend/src/hooks/useFlash.js
+++ frontend/src/hooks/useFlash.js
@@ -1,9 +1,9 @@
+// useFlash.js
 import bus from "../utils/bus";
 
 export default function useFlash() {
-
-    function setFlash(message,type) {
-
+    function setFlash(message, type) {
+        console.log("Emitindo evento flash:", message, type);
         bus.emit('flash', { message, type });
     }
     return { setFlash };
