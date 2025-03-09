c8d21a72626aa25a97bd76f5fcfa4f94a60a1ea3
Vitor Levi <160429477+DevVitorlevi@users.noreply.github.com>
2025-03-09 10:25:33 -0300

    Update useAuth.js

--- frontend/src/hooks/useAuth.js
+++ frontend/src/hooks/useAuth.js
@@ -1,40 +1,44 @@
 import { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';  // Adicione esta linha
+import { useNavigate } from 'react-router-dom';
 import api from '../utils/api';
 import useFlash from './useFlash';
 
 export default function useAuth() {
+    const [loading, setLoading] = useState(false);  // Estado de loading
     const { setFlash } = useFlash();
-    const [authed, setAuthed] = useState(false);  // Corrigido o uso do useState
+    const [authed, setAuthed] = useState(false);
     const navigate = useNavigate();
 
-    useEffect(()=>{
+    useEffect(() => {
         const token = localStorage.getItem('token');
         if (token) {
             api.defaults.headers.common['Authorization'] = `Bearer ${JSON.parse(token)}`;
             setAuthed(true);
         }
-    },[])
+    }, []);
 
     async function register(user) {
+        setLoading(true);  // Inicia o loading
         try {
             const response = await api.post('/user/register', user);
             setFlash('Cadastro realizado com sucesso!', 'success');
-            await authUser(response.data);  // Chame authUser com os dados recebidos
+            await authUser(response.data);
             return response.data;
 
         } catch (error) {
             const errorMessage = error.response?.data?.message || 'Erro ao cadastrar';
             setFlash(errorMessage, 'error');
             return null;
+        } finally {
+            setLoading(false);  // Finaliza o loading
         }
     }
 
     async function authUser(data) {
         setAuthed(true);
         localStorage.setItem('token', JSON.stringify(data.token));
-        navigate('/');  // Navegação após autenticação
+        navigate('/');
     }
 
-    return {authed, register };
+    return { authed, loading, register };
 }
