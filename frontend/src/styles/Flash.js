373ca8d472d0b76cfae6265c3aed115f08e0d60f
Vitor Levi <160429477+DevVitorlevi@users.noreply.github.com>
2025-03-09 10:25:41 -0300

    Update Flash.js

--- frontend/src/styles/Flash.js
+++ frontend/src/styles/Flash.js
@@ -4,34 +4,40 @@ import styled from 'styled-components';
 
 export const Message = styled.div`
     padding: 16px;
-    border-radius: 8px;
+    border-radius: 5px;
     font-size: 16px;
     font-weight: bold;
     color: white;
     display: flex;
     justify-content: center;
     align-items: center;
-    max-width: 500px;
+    width: 300px;
     position: fixed;
     top: 20px;
     right: 20px;
     z-index: 9999;
-    opacity: 0;
-    transform: translateY(-20px);
-    transition: opacity 0.3s ease-in-out, transform 0.3s ease-in-out;
+    animation: slideIn 4s;
 
+    @keyframes slideIn {    
+        0% {
+            transform: translateX(500px);
+        }
+        50%{
+            transform: translateX(0);
+        }
+        100% {
+            transform: translateX(500px);
+        }
+    }
     &.success {
         background-color: #28a745;
-        border: 2px solid #218838;
     }
 
-    &.error {
+    &.error { 
         background-color: #dc3545;
-        border: 2px solid #c82333;
     }
 
     &.visible {
-        opacity: 1;
         transform: translateY(0);
     }
 `;
