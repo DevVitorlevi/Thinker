2fd4a71c23f351bfec7ed1cfedb2bc8aa7e71905
Vitor Levi <160429477+DevVitorlevi@users.noreply.github.com>
2025-03-19 12:27:33 -0300

    Update Conteudo.js
    '

--- backend/source/models/Conteudo.js
+++ backend/source/models/Conteudo.js
@@ -1,10 +1,11 @@
-const mongoose = require('mongoose')
+// models/Conteudo.js
+const mongoose = require('mongoose');
 
-const Conteudo = new mongoose.Schema({
-titulo: { type: String, required: true },
-conceitos: { type: String, required: true },
-imagem: { type: String, required: true },
-materia: { type: mongoose.Schema.Types.ObjectId, ref: 'Materia' },
+const ConteudoSchema = new mongoose.Schema({
+    titulo: { type: String, required: true }, // Título do conteúdo
+    conceitos: { type: String, required: true }, // Descrição ou lista de conceitos
+    imagens: [{ type: String }], // Array de URLs ou caminhos das imagens
+    materia: { type: mongoose.Schema.Types.ObjectId, ref: 'Materia', required: true } // Referência à matéria
 }, { timestamps: true });
 
-module.exports = mongoose.model('Conteudo', Conteudo);
+module.exports = mongoose.model('Conteudo', ConteudoSchema);
