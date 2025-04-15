9ae6d2edeeffc2014c26c9358b42e0e8cbd70ffc
Vitor Levi <160429477+DevVitorlevi@users.noreply.github.com>
2025-04-15 19:32:58 -0300

    Model Questão

---
+++ Backend/src/models/Questao.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const Questao = new mongoose.Schema({
+    pergunta: { type: String, required: true },
+    alternativas: [{ type: String, required: true }],
+    respostaCorreta: { type: String, required: true },
+    quiz: { type: mongoose.Schema.Types.ObjectId, ref: 'Quiz' },
+    dificuldade: { type: String, enum: ['facil', 'medio', 'dificil'], default: 'medio' },
+    respondida_por: [{
+        user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+        acertou: { type: Boolean, default: false },
+        data: { type: Date, default: Date.now }
+    }]
+}, { timestamps: true });
+
+module.exports = mongoose.model('Questao', Questao);
