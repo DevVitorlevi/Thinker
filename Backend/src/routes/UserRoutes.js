a0017cfb2e8499a83a7913d08d32755b24c4c7a1
Vitor Levi <160429477+DevVitorlevi@users.noreply.github.com>
2025-04-30 11:30:46 -0300

    Update UserRoutes.js

--- Backend/src/routes/UserRoutes.js
+++ Backend/src/routes/UserRoutes.js
@@ -5,9 +5,7 @@ const VerifyToken = require('../helpers/verify-token');
 const { ImageUpload } = require('../helpers/image-up');
 const QuestaoController = require('../controllers/QuestionsController');
 const QuizController = require('../controllers/QuizController');
-const ConteudoController = require('../controllers/ConteudoController');
-const RankingController = require('../controllers/RankingController');
- 
+
 // Auth
 router.post('/register', UserController.register);
 router.post('/login', UserController.login);
@@ -20,12 +18,5 @@ router.patch('/edit/:id', VerifyToken, ImageUpload.single('image'), UserControll
 // Quiz Interactions
 router.post('/responder', VerifyToken, QuestaoController.responderQuestao);
 router.post('/completar-quiz', VerifyToken, QuizController.completarQuiz);
-
-router.get('/conteudos/:materiaId', VerifyToken, ConteudoController.getByMateria);
-router.get('/conteudo/:id', VerifyToken, ConteudoController.getById);
-
-// Ranking Routes
-router.get('/ranking', VerifyToken, RankingController.getRanking);
-router.get('/rankings', VerifyToken, RankingController.getAllRankings);
 
 module.exports = router;
