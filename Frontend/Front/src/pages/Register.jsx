0dc4b5ea73ba7abe8a19260a53056a209c40db95
Vitor Levi <160429477+DevVitorlevi@users.noreply.github.com>
2025-04-30 11:30:57 -0300

    Create Register.jsx

---
+++ Frontend/Front/src/pages/Register.jsx
@@ -0,0 +1,12 @@
+import { Main, Image } from "../styles/Register"
+import Login from '../assets/login.png';
+const Register = () => {
+
+    return (
+        <>
+            <Main>
+            </Main>
+        </>
+    )
+}
+export default Register
