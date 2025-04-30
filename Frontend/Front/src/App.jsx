2dab88a089b6dfd2284caf479ce13263c5f41541
Vitor Levi <160429477+DevVitorlevi@users.noreply.github.com>
2025-04-30 11:30:50 -0300

    Update App.jsx

--- Frontend/Front/src/App.jsx
+++ Frontend/Front/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { GlobalStyle } from "./styles/Global"
 import { Landing } from "./pages/Landing"
+import Register from './pages/Register'
 function App() {
 
 
@@ -10,6 +11,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Landing />} />
+          <Route path='/register' element={<Register />} />
         </Routes>
       </BrowserRouter>
     </>
