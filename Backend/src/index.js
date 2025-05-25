286189e39e5fa8cb850f85af1e182b81cb3acf3c
Vitor Levi <160429477+DevVitorlevi@users.noreply.github.com>
2025-05-25 09:54:46 -0300

    att

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -15,7 +15,7 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+  origin: process.env.FRONTEND_URL || 'http://localhost:5174',
   credentials: true
 }));
 
