e1a3f3dbaef0a81d51f0f8724ba517e7ebc36b39
Vitor Levi <160429477+DevVitorlevi@users.noreply.github.com>
2025-06-07 10:36:14 -0300

    Fix: Problema no Cadastro Resolvido

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -15,7 +15,7 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5174',
+  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
   credentials: true
 }));
 
