fc212acb97ef370194b8f995ea8f0049279fe9cf
Vitor Levi <160429477+DevVitorlevi@users.noreply.github.com>
2025-06-11 07:25:47 -0300

    feat: Adicionado o Sistema de Patente

--- Backend/src/models/User.js
+++ Backend/src/models/User.js
@@ -1,11 +1,9 @@
 const mongoose = require('mongoose');
 
-const EstatsticasSchema = new mongoose.Schema({
+const EstatisticasSchema = new mongoose.Schema({
     questoes_feitas: { type: Number, default: 0 },
     acertos: { type: Number, default: 0 },
     quizzes_completos: { type: Number, default: 0 },
-    pontos: { type: Number, default: 0 }, // Adicionado campo de pontos
-    patente: { type: String, default: 'Aprendiz do Conhecimento' } // Adicionado campo de patente
 });
 
 const UserSchema = new mongoose.Schema({
@@ -14,13 +12,25 @@ const UserSchema = new mongoose.Schema({
     password: { type: String, required: true },
     image: { type: String },
     role: { type: String, enum: ['admin', 'user'], default: 'user' },
-    estatisticas: { type: EstatsticasSchema, default: () => ({}) },
+    pontos: { type: Number, default: 0 }, // Pontos acumulados
+    patente: { 
+        type: String, 
+        enum: [
+            'Aprendiz do Conhecimento',
+            'Decifrador de Desafios',
+            'Mestre dos Quizzes',
+            'Gênio da Mente Dourada',
+            '🧠 O THINKER'
+        ],
+        default: 'Aprendiz do Conhecimento'
+    },
+    estatisticas: { type: EstatisticasSchema, default: () => ({}) },
     quizzes_respondidos: [{
         quiz: { type: mongoose.Schema.Types.ObjectId, ref: 'Quiz' },
         data: { type: Date, default: Date.now },
         acertos: { type: Number, default: 0 },
         total_questoes: { type: Number, default: 0 },
-        pontos_ganhos: { type: Number, default: 0 } // Adicionado pontos ganhos por quiz
+        pontos_ganhos: { type: Number, default: 0 } // Pontos ganhos naquele quiz
     }]
 }, { timestamps: true });
 
