48d71bbc26e58050e00e0440c0b691280950e25b
Vitor Levi <160429477+DevVitorlevi@users.noreply.github.com>
2025-06-11 18:23:47 -0300

    Refactor: Refatorando o Modelo de Questao e Quiz

--- Backend/src/models/Questions.js
+++ Backend/src/models/Questions.js
@@ -6,11 +6,6 @@ const Questao = new mongoose.Schema({
     respostaCorreta: { type: String, required: true },
     quiz: { type: mongoose.Schema.Types.ObjectId, ref: 'Quiz' },
     dificuldade: { type: String, enum: ['facil', 'medio', 'dificil'] },
-    respondida_por: [{ 
-        user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-        acertou: { type: Boolean, default: false },
-        data: { type: Date, default: Date.now } 
-    }]
 }, { timestamps: true });
 
 module.exports = mongoose.model('Questao', Questao);
