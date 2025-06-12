7721b24bc5a6fc092d2541a9dc8074062f634713
Vitor Levi <160429477+DevVitorlevi@users.noreply.github.com>
2025-06-12 15:44:04 -0300

    Fix: Resolvendo o erro no Cors

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -14,8 +14,18 @@ const port = process.env.PORT || 5000;
 // Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+const allowedOrigins = ['http://localhost:5173', 'http://localhost:5174'];
+
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  origin: function (origin, callback) {
+    
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true
 }));
 
