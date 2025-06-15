6fabe26dc2cd900f2c29d511f9e96d15a96bb43c
Vitor Levi <levivitor8@gmail.com>
2025-06-15 11:34:51 -0300

    Server: Adicionado o Mongo DB Atlas

--- Backend/src/config/conn.js
+++ Backend/src/config/conn.js
@@ -1,11 +1,16 @@
 require('dotenv').config({ path: require('path').resolve(__dirname, '../../.env') });
-console.log('🧪 DB_URI:', process.env.DB_URI || 'mongodb://127.0.0.1:27017/thinker'); // ADICIONE ISTO AQUI
 
 const mongoose = require('mongoose');
 
+console.log('🧪 DB_URI:', process.env.DB_URI);
+
 async function main() {
-  await mongoose.connect(process.env.DB_URI || 'mongodb://127.0.0.1:27017/thinker');
-  console.log('✅ Conectado ao MongoDB!');
+  try {
+    await mongoose.connect(process.env.DB_URI);
+    console.log('✅ Conectado ao MongoDB Atlas com sucesso!');
+  } catch (err) {
+    console.error('❌ Erro ao conectar ao MongoDB:', err);
+  }
 }
 
-main().catch(err => console.error('❌ Erro ao conectar ao MongoDB:', err));
+main();
